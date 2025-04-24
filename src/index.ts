dfe9daae2d2bbcc8b619486203f6b326c039748d
Twyco <twyco@icloud.com>
2025-04-24 21:28:02 +0200

    renamed Image Type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 import ImagePicker from './components/ImagePicker.vue';
 
-export type { ImageType } from './types/image-interface.ts'
+export type { Image } from './types/image.ts'
 
 export {
   ImagePicker
